0be4400b01d5e6fb9ed963d8d0d25aaf86a5d18a
Aina Adoption Oluwasomidotun <ainaadoption@gmail.com>
2025-11-17 02:12:49 +0100

    Import modules and classes in __init__.py

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,1 +1,6 @@
 
+from .module import Module, Sequential
+from .layers import Linear, Sigmoid, ReLU
+from .losses import MSELoss, CrossEntropyLoss
+from .optimizers import SGD
+from .model_utils import save_model, load_model
